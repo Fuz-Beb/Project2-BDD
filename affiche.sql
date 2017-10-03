5ec2f0ace48733070a417b43b62015e5fc4cb44e
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-10-03 16:17:36 -0600

    Update script

---
+++ affiche.sql
@@ -0,0 +1,24 @@
+﻿-- Affichage de l'ensemble des tables de la base de donnée --
+
+-- Affichage de la table Avocat
+SELECT * FROM "Avocat"
+
+UNION
+-- Affichage de la table Juge
+SELECT * FROM "Juge"
+
+UNION
+-- Affichage de la table Jury
+SELECT * FROM "Jury"
+
+UNION
+-- Affichage de la table Partie
+SELECT * FROM "Partie"
+
+UNION
+-- Affichage de la table Proces
+SELECT * FROM "Proces"
+
+UNION
+-- Affichage de la table Seance
+SELECT * FROM "Seance";
